3ade0c891f5f567b913d4650afdb5b3c88b6335e
Blair Preston <blair.preston@student.awesomeinc.org>
2022-06-07 17:41:01 +0000

    set up new meetup form and can see it console log the input

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,15 @@
 // domian.com/new-meetup
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData){
+        console.log(enteredMeetupData);
+    }
+
+    return (
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    )
+}
+
+export default NewMeetupPage;
