11a22ee7a570d766303eebeaa3eaa9454d8741e7
Blair Preston <blair.preston@student.awesomeinc.org>
2022-06-07 17:35:39 +0000

    organize pages folder for new-meetup, index, and meetup info pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,3 @@
+// dynamically rendered page for multiple meetups with different id's
+// id's should be part of the url
+// and then when we load the page we want to use the id to fetch and display the appropriate data
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+// domain.com/
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://via.placeholder.com/350x150',
+        address: 'Some address 1, city, state',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm1',
+        title: 'A Second Meetup',
+        image: 'https://via.placeholder.com/350x150',
+        address: 'Some address 2, city, state',
+        description: 'This is a second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'A Third Meetup',
+        image: 'https://via.placeholder.com/350x150',
+        address: 'Some address 3, city, state',
+        description: 'This is a third meetup'
+    }
+]
+
+
+function HomePage() {
+  return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+  )
+}
+
+export default HomePage;
