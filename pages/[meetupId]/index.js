50a3d1c4a533a28372550c40748913aa4a8a5516
Blair Preston <blair.preston@student.awesomeinc.org>
2022-06-07 17:59:24 +0000

    add details page for individual meet up

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,18 @@
 // dynamically rendered page for multiple meetups with different id's
 // id's should be part of the url
 // and then when we load the page we want to use the id to fetch and display the appropriate data
+
+import { Fragment } from "react";
+
+function MeetupDetails() {
+    return (
+        <Fragment>
+            <img src="https://via.placeholder.com/350x150" alt="meetup image"></img>
+            <h1>A first meetup</h1>
+            <address>Somestreet 1</address>
+            <p>The description</p>
+        </Fragment>
+    )
+}
+
+export default MeetupDetails;
