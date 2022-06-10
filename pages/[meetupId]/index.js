6b0139d69cb05a4bf50d1850acc8c3540986ae10
Blair Preston <blair.preston@student.awesomeinc.org>
2022-06-10 16:07:03 +0000

    getStaticPaths function for the single page meetups

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -15,4 +15,48 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    //returns an object that describes all possible pages that need to be 
+    // pre-generated
+    // would also be fetched from database, but hardcoded for now
+    // fallback: false -- pre generate these pages and give a 404 error if
+    // a request for any other pages come in
+    // fallback: true, pre-generate the following pages and also generate othe
+    // paths. when the request comes in
+    return {
+        fallback: false,
+        paths: [
+            { 
+                params: {
+                    meetupId: 'm1'
+                },
+            },
+            { 
+                params: {
+                    meetupId: 'm2'
+                },
+            },
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    // use context to know which single meetup data to be fetching
+    const meetupId = context.params.meetupId;
+    console.log(meetupId)
+
+    return {
+        props: {
+            meetupData: {
+                image:'https://via.placeholder.com/350x150',
+                id: meetupId,
+                title: 'First Meetup',
+                address: 'Some Street 1',
+                description:'This is a first meetup',
+            }
+        }
+    }
+}
 export default MeetupDetails;
