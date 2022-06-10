289f2d182ce3ae6ffe553fc317cfaad72e6cd312
Blair Preston <blair.preston@student.awesomeinc.org>
2022-06-10 15:39:02 +0000

    fetch the dummy data statically so the page is prerendered

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 // domain.com/
-
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -27,10 +26,19 @@ const DUMMY_MEETUPS = [
 ]
 
 
-function HomePage() {
+function HomePage(props) {
   return (
-    <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={props.meetups} />
   )
+}
+
+export async function getStaticProps() {
+  //fetch data from API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  };
 }
 
 export default HomePage;
